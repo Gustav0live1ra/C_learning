f8de0ed71fb78a0ba86fa38b2f2f5fdabf404bdb
Gustavo <gustavoliveira8188@gmail.com>
2025-11-03 17:51:44 -0300

    maked initials programs in C

---
+++ code1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+
+                                            //two numbers sum//
+int main()
+{
+    int n1, n2;
+    printf("digite um número: ");
+    scanf("%d", &n1);
+    printf("digite outro número: ");
+    scanf("%d", &n2);
+    printf("a soma dos dois números é igual a: %d", n1 + n2);
+    return 0;
+}
